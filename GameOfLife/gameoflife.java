5398f17a02d35626a334df0f792a3f1169557599
FlyingSaturn <56539009+FlyingSaturn@users.noreply.github.com>
2024-05-11 22:35:09 +0000

    Some minor changes pt. 2

--- GameOfLife/gameoflife.java
+++ GameOfLife/gameoflife.java
@@ -1,15 +1,15 @@
 class gameoflife
 {
-    String A[][] = new String[15][15];String B[][] = new String[15][15];//you can choose the subscript of your choice
-    //To initialize and to show the starting screen
+    String A[][] = new String[15][15];String B[][] = new String[15][15]; // you can choose the subscript of your choice
+    // To initialize and to show the starting screen
     void initialize() throws InterruptedException
     {
-        System.out.print('\f');//clearing the screen
+        System.out.print('\f'); // clearing the screen
         for(int i = 0; i < A.length; i++)
         {
             for(int j = 0; j < A[0].length; j++)
             {
-                if(Math.round(Math.random() * .75) == 1)//.75 was just an arbitrary number to empty the space a bit
+                if(Math.round(Math.random() * .75) == 1) // .75 was just an arbitrary number to empty the space a bit
                     A[i][j] = "⬛";
                 else
                     A[i][j] = "⬜";
@@ -26,7 +26,7 @@ class gameoflife
         System.out.print(".");
         Thread.sleep(1000);
     }
-    //the main part of the code: can a block(ides, jdes) be given life?
+    // the main part of the code: can a block(ides, jdes) be given life?
     boolean breedable(int ides, int jdes)
     {
         int neighbours = 0;
@@ -34,27 +34,27 @@ class gameoflife
         {
             for(int j = jdes - 1; j <= jdes + 1; j++)
                 if((i != ides || j != jdes) && j > -1 && j < A[0].length && i > -1 && i < A.length)
-                {//neighbours. neighbours can't have negative indexes or indexes beyond range
-                //but the coords have to be different from the main block (ides, jdes)
+                { // neighbours. neighbours can't have negative indexes or indexes beyond range
+                // but the coords have to be different from the main block (ides, jdes)
                     if(isAlive(i, j))
                         neighbours++;
                 }
         }
-        if((neighbours == 2 || neighbours == 3) && isAlive(ides, jdes))//the rule of game of life!!!
-            return true;//"keep them alive." The change variable in void display() could've been avoided if I did something for this
+        if((neighbours == 2 || neighbours == 3) && isAlive(ides, jdes)) // the rule of game of life!!!
+            return true; // "keep them alive." The change variable in void display() could've been avoided if I did something for this
         else if(neighbours == 3 && !isAlive(ides, jdes))
-            return true;//create...
+            return true; // create...
         else
             return false;
     }
-    //is the specific block even alive in the first place?
+    // is the specific block even alive in the first place?
     boolean isAlive(int i, int j)
     {
         if(A[i][j].equals("⬛"))
             return true;
         return false;
     }
-    //handles what should be done to the block after knowing if the block is breedable
+    // handles what should be done to the block after knowing if the block is breedable
     boolean birthAndDeath(int ides, int jdes)
     {
         boolean wasThisNeeded = false;
@@ -69,9 +69,9 @@ class gameoflife
             B[ides][jdes] = "⬛";
             wasThisNeeded = true;
         }
-        return wasThisNeeded;//for the variable change in void display()
+        return wasThisNeeded; // for the variable change in void display()
     }
-    //displays the blocks continuously
+    // displays the blocks continuously
     void display() throws InterruptedException
     {
         boolean change;
@@ -88,7 +88,7 @@ class gameoflife
             {
                 for(int j = 0; j < A[0].length; j++)
                 {
-                    if(birthAndDeath(i, j))//if it isn't in the same state like in the previous iteration
+                    if(birthAndDeath(i, j)) // if it isn't in the same state like in the previous iteration
                         change = true;
                 }
             }
@@ -101,10 +101,10 @@ class gameoflife
                 }
                 System.out.println();
             }          
-            Thread.sleep(105);//105 ms
-        } while(change);//if the  last step is the same as the current step, then the code will end
+            Thread.sleep(105); // 105 ms
+        } while(change); // if the last step is the same as the current step, then the code will end
     }
-    //main() method
+    // main() method
     public static void main(String args[]) throws InterruptedException
     {
         gameoflife obj = new gameoflife();
