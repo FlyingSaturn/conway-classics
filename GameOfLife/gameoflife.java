4c1a6c36981ec1cff25ecf118854ab853e89e684
FlyingSaturn <56539009+FlyingSaturn@users.noreply.github.com>
2024-05-12 15:09:31 +0530

    Update gameoflife.java

--- GameOfLife/gameoflife.java
+++ GameOfLife/gameoflife.java
@@ -1,15 +1,16 @@
 class gameoflife
 {
-    String A[][] = new String[15][15];String B[][] = new String[15][15]; // you can choose the subscript of your choice
+    String A[][] = new String[15][15];
+    String B[][] = new String[15][15]; // you can choose the subscript of your choice
     // To initialize and to show the starting screen
     void initialize() throws InterruptedException
     {
         System.out.print('\f'); // clearing the screen
-        for(int i = 0; i < A.length; i++)
+        for (int i = 0; i < A.length; i++)
         {
-            for(int j = 0; j < A[0].length; j++)
+            for (int j = 0; j < A[0].length; j++)
             {
-                if(Math.round(Math.random() * .75) == 1) // .75 was just an arbitrary number to empty the space a bit
+                if (Math.round(Math.random() * .75) == 1) // .75 was just an arbitrary number to empty the space a bit
                     A[i][j] = "⬛";
                 else
                     A[i][j] = "⬜";
@@ -30,19 +31,19 @@ class gameoflife
     boolean breedable(int ides, int jdes)
     {
         int neighbours = 0;
-        for(int i = ides - 1; i <= ides + 1; i++)
+        for (int i = ides - 1; i <= ides + 1; i++)
         {
-            for(int j = jdes - 1; j <= jdes + 1; j++)
-                if((i != ides || j != jdes) && j > -1 && j < A[0].length && i > -1 && i < A.length)
+            for (int j = jdes - 1; j <= jdes + 1; j++)
+                if ((i != ides || j != jdes) && j > -1 && j < A[0].length && i > -1 && i < A.length)
                 { // neighbours. neighbours can't have negative indexes or indexes beyond range
                 // but the coords have to be different from the main block (ides, jdes)
-                    if(isAlive(i, j))
+                    if (isAlive(i, j))
                         neighbours++;
                 }
         }
-        if((neighbours == 2 || neighbours == 3) && isAlive(ides, jdes)) // the rule of game of life!!!
+        if ((neighbours == 2 || neighbours == 3) && isAlive(ides, jdes)) // the rule of game of life!!!
             return true; // "keep them alive." The change variable in void display() could've been avoided if I did something for this
-        else if(neighbours == 3 && !isAlive(ides, jdes))
+        else if (neighbours == 3 && !isAlive(ides, jdes))
             return true; // create...
         else
             return false;
@@ -50,7 +51,7 @@ class gameoflife
     // is the specific block even alive in the first place?
     boolean isAlive(int i, int j)
     {
-        if(A[i][j].equals("⬛"))
+        if (A[i][j].equals("⬛"))
             return true;
         return false;
     }
@@ -59,12 +60,12 @@ class gameoflife
     {
         boolean wasThisNeeded = false;
         boolean breed = breedable(ides, jdes);
-        if(!breed && A[ides][jdes].equals("⬛"))
+        if (!breed && A[ides][jdes].equals("⬛"))
         {    
             B[ides][jdes] = "⬜";
             wasThisNeeded = true;
         }
-        else if(breed && A[ides][jdes].equals("⬜"))
+        else if (breed && A[ides][jdes].equals("⬜"))
         {
             B[ides][jdes] = "⬛";
             wasThisNeeded = true;
@@ -75,26 +76,26 @@ class gameoflife
     void display() throws InterruptedException
     {
         boolean change;
-        for(int i = 0; i < A.length; i++)
+        for (int i = 0; i < A.length; i++)
         {
-            for(int j = 0; j < A[0].length; j++)
+            for (int j = 0; j < A[0].length; j++)
                 B[i][j] = A[i][j];
         }
         do
         {
             System.out.print('\f');
             change = false;
-            for(int i = 0; i < A.length; i++)
+            for (int i = 0; i < A.length; i++)
             {
-                for(int j = 0; j < A[0].length; j++)
+                for (int j = 0; j < A[0].length; j++)
                 {
-                    if(birthAndDeath(i, j)) // if it isn't in the same state like in the previous iteration
+                    if (birthAndDeath(i, j)) // if it isn't in the same state like in the previous iteration
                         change = true;
                 }
             }
-            for(int i = 0; i < A.length; i++)
+            for (int i = 0; i < A.length; i++)
             {
-                for(int j = 0; j < A[0].length; j++)
+                for (int j = 0; j < A[0].length; j++)
                 {
                     A[i][j] = B[i][j];
                     System.out.print(B[i][j] + " ");
